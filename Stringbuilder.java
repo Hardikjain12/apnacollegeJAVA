8f030f51f77d4b0770bcca3644beb4393d8d258d
Hardikjain12 <hardikj714@gmail.com>
2024-03-20 17:34:55 +0530

    new

--- Stringbuilder.java
+++ Stringbuilder.java
@@ -1,27 +1,38 @@
 import java.util.*;
 
+@SuppressWarnings("unused")
 public class Stringbuilder {
 
     public static void main(String[] args) {
-        StringBuilder sc = new StringBuilder("Hardik");
-        System.out.println(sc);
+        // StringBuilder sc = new StringBuilder("Hardik");
+        // System.out.println(sc);
 
-        // character at index 0
-        System.out.println(sc.charAt(0));
+        // // character at index 0
+        // System.out.println(sc.charAt(0));
 
-        // set char at index
-        sc.setCharAt(0, 'p');
-        System.out.println(sc);
+        // // set char at index
+        // sc.setCharAt(0, 'p');
+        // System.out.println(sc);
         
-        // insert
-        sc.insert(0, 'G');
-        System.out.println(sc);
-        sc.insert(2, 'G');
-        System.out.println(sc);
+        // // insert
+        // sc.insert(0, 'G');
+        // System.out.println(sc);
+        // sc.insert(2, 'G');
+        // System.out.println(sc);
 
-        // delete
-        sc.delete(0, 3);
-        System.out.println(sc);
+        // // delete
+        // sc.delete(0, 3);
+        // System.out.println(sc);
+
+        // append
+        StringBuilder sb = new StringBuilder("h");
+        sb.append("e");
+        sb.append("l");
+        sb.append("l");
+        sb.append("o");
+
+        System.out.println(sb);
+
     }
 }
 
